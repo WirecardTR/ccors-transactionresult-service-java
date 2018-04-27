63ba921e5e8288f2583a2fcdd3f3cf52b726210f
Coşkun Kurtuldu <32268994+CoskunKurtuldu@users.noreply.github.com>
2018-04-28 01:26:25 +0300

    Create input.java

---
+++ src/java/wirecard/input.java
@@ -0,0 +1,15 @@
+import java.util.Date;
+import java.util.UUID;
+public class input {
+  
+    public int StatusCode;
+    public String ResultCode;
+    public String ResultMessage;
+    public String OrderId;
+    public String MPAY;
+    public double Price;
+    public Date TransactionDate;
+    public String ServiceTypeCode;
+    public String TransactionTypeCode;
+    public String ExtraParam;
+}
