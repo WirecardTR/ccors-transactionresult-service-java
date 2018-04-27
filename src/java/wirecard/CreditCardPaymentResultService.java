7b001d377c56b94f2e219222fe485acc501c1c8d
Coşkun Kurtuldu <32268994+CoskunKurtuldu@users.noreply.github.com>
2018-04-28 01:25:25 +0300

    Update CreditCardPaymentResultService.java

--- src/java/wirecard/CreditCardPaymentResultService.java
+++ src/java/wirecard/CreditCardPaymentResultService.java
@@ -7,27 +7,14 @@ import javax.jws.WebMethod;
 import javax.jws.WebParam;
 
 
-@WebService(serviceName = "CreditCardPaymentResultService")
+@WebService(serviceName = "CreditCardPaymentResultService",targetNamespace = "http://cservices.mikro-odeme.com.tr/mapping/generated")
 public class CreditCardPaymentResultService {
 
     /**
      * This is a sample web service operation
      */
     @WebMethod(operationName = "ReceiveTransactionResult")
-    public Response ReceiveTransactionResult
-        (
-            @WebParam(name = "StatusCode") int StatusCode,
-            @WebParam(name = "ResultCode") String ResultCode,
-            @WebParam(name = "ResultMessage") String ResultMessage,
-            @WebParam(name = "OrderId") String OrderId,
-            @WebParam(name = "MPAY") String MPAY,
-            @WebParam(name = "Price") double Price,
-            @WebParam(name = "TransactionDate") Date TransactionDate,
-            @WebParam(name = "ServiceTypeCode") String ServiceTypeCode,
-            @WebParam(name = "TransactionTypeCode") String TransactionTypeCode,
-            @WebParam(name = "ExtraParam") String ExtraParam
-    ) 
-    
+    public Response ReceiveTransactionResult(@WebParam(name = "input") input input )
     {
         return null;
     }
